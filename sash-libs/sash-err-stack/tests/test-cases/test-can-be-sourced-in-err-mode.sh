fc0f805db3c124e2590f49b3a3cdcf88d0960675
Cynthia <cynthia@coan.dev>
2025-09-09 23:07:15 +0000

    Migration

--- sash-libs/sash-err-stack/tests/test-cases/test-can-be-sourced-in-err-mode.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-source "$SCRIPT_DIR/../../sash-err-stack.sh"
-
-false
-echo "hey"
--- sash-libs/sash-err-stack/tests/test-cases/test-source-multiple-times.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-source "$SCRIPT_DIR/../../sash-err-stack.sh"
-source "$SCRIPT_DIR/../../sash-err-stack.sh"
